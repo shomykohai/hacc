5eac6c4cd4cbd724349e6efe6b36d73ec079bd70
shomy <git@itssho.my>
2026-09-04 14:09:23 +0200

    tests: Use correct test name for V5 DA

--- tests/da/v5.rs
+++ tests/da/v5.rs
@@ -6,7 +6,7 @@ const DA_V5: &[u8] = include_bytes_aligned!(4, "../files/DA_V5.bin");
 const DA_DESC: &str = "MTK_AllInOne_DA_v3.3001.2023/02/20.16:47_590669";
 
 #[test]
-fn da_v6_parse() {
+fn da_v5_parse() {
     let da = Da::try_read(DA_V5).expect("Failed to read DA V5 header");
 
     assert_eq!(da.header().id(), "MTK_DOWNLOAD_AGENT");
